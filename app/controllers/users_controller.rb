362af06e106523a1bff4ee08be7bcc3ae67221c7
takaba <ec2-user@ip-172-31-34-107.ap-southeast-1.compute.internal>
2018-05-06 03:13:05 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save  # => Validation
+      # Success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # GET "users/#{@user.id}" => show 
+    else
+      # Failure
+      render 'new'
+    end
+  end
+  
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password,
+      :password_confirmation)
   end
 end
