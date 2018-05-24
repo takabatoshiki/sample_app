bb96f764590d067a4657ee04836289a10f07b213
takaba <ec2-user@ip-172-31-34-107.ap-southeast-1.compute.internal>
2018-05-24 12:40:24 +0000

    Finish ch12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum:6 }, allow_nil: true
+  validates :password, presence: true, length: { minimum:6 } , allow_nil: true
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -45,15 +45,28 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
   
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+  
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
   
   private
   
     def downcase_email
-      self.email = self.email.downcase
+      self.email = email.downcase
     end
     
     def create_activation_digest
       self.activation_token = User.new_token
-      self.activation_digest = User.digest(self.activation_token)
+      self.activation_digest = User.digest(activation_token)
     end
 end
