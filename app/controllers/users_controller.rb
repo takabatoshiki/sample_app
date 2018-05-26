9a312532d116a8be7415c0bcd2f5b25a61f905fc
takaba <ec2-user@ip-172-31-34-107.ap-southeast-1.compute.internal>
2018-05-26 23:52:05 +0000

    Finish ch13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/:id
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   # GET /users/new
@@ -65,14 +66,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(
         :name, :email, :password,
         :password_confirmation)
-    end
-    
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in"
-        redirect_to login_url
-      end
     end
     
     def correct_user
